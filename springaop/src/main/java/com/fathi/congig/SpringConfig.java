95c96f973c94009fab3b1fea42edfc2736bfa6af
AMIR FATHI <fathiamir37@gmail.com>
2022-10-12 16:55:10 +0330

    Add files via upload

---
+++ springaop/src/main/java/com/fathi/congig/SpringConfig.java
@@ -0,0 +1,9 @@
+package com.fathi.congig;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.fathi")
+public class SpringConfig {
+}
