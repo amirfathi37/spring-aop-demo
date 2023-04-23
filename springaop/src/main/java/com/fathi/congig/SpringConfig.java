e2b1833d29bc8112cbdebe726fc86b8689a5d0f1
AMIR FATHI <fathiamir37@gmail.com>
2023-04-23 12:42:57 +0200

    clean

--- springaop/src/main/java/com/fathi/congig/SpringConfig.java
+++
@@ -1,9 +0,0 @@
-package com.fathi.congig;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.fathi")
-public class SpringConfig {
-}
